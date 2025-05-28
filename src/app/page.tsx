c9074034fb1931bb5bc12f131bce9ed120cf7f0f
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-05-28 19:50:37 +0500

    dark:text-white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 export default function Home() {
   return (
   <div className="space-y-4 text-black">
-      <div className="text-xl font-bold">Welcome to: CyberDevs</div>
+      <div className="text-xl font-bold dark:text-white">Welcome to: CyberDevs</div>
       <div className="space-y-2 rounded p-4 h-100 shadow">
       </div>
       <form  className="flex">
         <input
           type="text"
           placeholder="Type your message here..."
-          className="flex-grow  p-2 rounded text-black"
+          className="flex-grow  p-2 rounded text-black dark:text-white"
         />
         <button type="submit" className="px-4 py-2 bg-green-500 rounded">
           Send
